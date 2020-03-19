763089ec16955d53aa00737eec32436e7bac506d
janlycn <janlycn@gmail.com>
2020-03-19 15:17:29 +0800

    Complete Chinese Translation

--- lang/zh-cn/lang.php
+++ lang/zh-cn/lang.php
@@ -2,11 +2,11 @@
 
 return [
     'plugin' => [
-        'name' => 'Location',
-        'description' => 'Location based features, such as Country and State.'
+        'name' => '位置',
+        'description' => '位置基础特征，比如省/地区'
     ],
     'permissions' => [
-        'settings' => 'Locations management',
+        'settings' => '位置管理',
     ],
     'location' => [
         'label' => '位置信息',
@@ -23,10 +23,18 @@ return [
         'enabled_help' => '被禁用后不会继续在前端显示.',
         'enable_or_disable_title' => "启用 或 禁用",
         'enable_or_disable' => '启用 或 禁用',
-        'selected_amount' => '已选项: :amount',
+        'selected_amount' => '已选择: :amount',
         'enable_success' => '成功启用所选项.',
         'disable_success' => '成功禁用所选项.',
         'disable_confirm' => '确认禁用该项?',
+        'unpin_label' => '取消锁定',
+        'pinned_label' => '锁定',
+        'pinned_help' => '锁定的位置排在列表的第一位。',
+        'pin_or_unpin_title' => "锁定或取消锁定位置",
+        'pin_or_unpin' => '锁定或取消锁定',
+        'pin_success' => '已成功锁定选择的位置。',
+        'unpin_success' => '已成功解锁选定的位置。',
+        'unpin_confirm' => '确定吗？',
         'list_title' => '管理位置信息',
         'delete_confirm' => '确认删除该记录?',
         'return_to_list' => '返回位置列表',
@@ -34,6 +42,13 @@ return [
         'default_country_comment' => '用户未设置国家时系统默认国家.',
         'default_state' => '省市/地区',
         'default_state_comment' => '用户未选择地区时默认使用省市/地区.',
+    ],
+    'settings' => [
+        'menu_label' => '位置设定',
+        'menu_description' => '管理位置基础设定.',
+        'google_maps_key' => 'Google Maps API Key',
+        'google_maps_key_comment' => '如果你计划使用谷歌地图服务，请在这里输入 API Key。',
+        'credentials_tab' => '证书',
     ],
     'state' => [
         'label' => '省市/地区',
@@ -49,6 +64,7 @@ return [
         'select' => '-- 选择国家 --',
         'code' => '代码',
         'code_comment' => '输入国家唯一编码.',
-        'enabled' => '启用'
+        'enabled' => '启用',
+        'pinned' => '锁定',
     ]
 ];
